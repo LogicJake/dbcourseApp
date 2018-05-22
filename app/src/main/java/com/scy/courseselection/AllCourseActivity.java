b163b114f10b1e90dcde8973a2a7933bd6a9dff8
NepoleonAsus <835410808@qq.com>
2018-05-22 12:07:10 +0800

    增删查改

--- app/src/main/java/com/scy/courseselection/AllCourseActivity.java
+++ app/src/main/java/com/scy/courseselection/AllCourseActivity.java
@@ -1,14 +1,18 @@
 package com.scy.courseselection;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -31,7 +35,7 @@ import okhttp3.Call;
 import static com.scy.courseselection.AllStuActivity.setListViewHeightBasedOnChildren;
 import static com.scy.courseselection.MainActivity.api_url;
 
-public class AllCourseActivity extends AppCompatActivity implements SpringView.OnFreshListener,AdapterView.OnItemClickListener {
+public class AllCourseActivity extends AppCompatActivity implements SpringView.OnFreshListener,AdapterView.OnItemClickListener,View.OnClickListener {
     private Context context = this;
     private SpringView sv;
     private int page = 1;
@@ -40,6 +44,7 @@ public class AllCourseActivity extends AppCompatActivity implements SpringView.O
     List<HashMap<String, Object>> mListData  = new ArrayList<HashMap<String, Object>>();;
     private CourseAdapter courseAdapter;
     private static final String TAG = "AllCourseActivity";
+    private FloatingActionButton floatingActionButton;
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -70,8 +75,10 @@ public class AllCourseActivity extends AppCompatActivity implements SpringView.O
 
         mlistview = (ListView)findViewById(R.id.booklist) ;
         mlistview.setDividerHeight(5 );
-
         mlistview.setOnItemClickListener(this);
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(this);
 
         getData();
     }
@@ -141,5 +148,69 @@ public class AllCourseActivity extends AppCompatActivity implements SpringView.O
         Intent intent = new  Intent(AllCourseActivity.this,AllScActivity.class);
         intent.putExtra("cno",cno);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab:{
+                View dialog_view = getLayoutInflater().inflate(R.layout.dialog_view_add_course, null);
+                final EditText cno = (EditText) dialog_view.findViewById(R.id.dialog_cno);
+                final EditText cname = (EditText) dialog_view.findViewById(R.id.dialog_cname);
+                final EditText credit = (EditText) dialog_view.findViewById(R.id.dialog_ccredit);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("添加课程")
+                        .setView(dialog_view)
+                        .setCancelable(false)
+                        .setNegativeButton("取消",null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String content_cno = cno .getText().toString();
+                                String content_cname = cname .getText().toString();
+                                String content_credit = credit .getText().toString();
+                                addCourse(content_cno,content_cname,content_credit);
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+                break;
+            }
+        }
+    }
+
+    public void addCourse(String cno,String cname,String credit){
+        OkHttpUtils
+                .get()
+                .url(api_url)
+                .addParams("_action", "insertCourse")
+                .addParams("cno", cno)
+                .addParams("cname", cname)
+                .addParams("ccredit",credit)
+                .build()
+                .execute(new StringCallback()
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Toast.makeText(context,"网络错误",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.d(TAG, "onResponse: "+response);
+                        try {
+                            JSONObject res = new JSONObject(response).getJSONObject("data");
+                            int status = res.getInt("status");
+                            if(status == 1)
+                                Toast.makeText(context,"添加成功",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(context,"添加失败",Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context,"服务器错误",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/scy/courseselection/AllStuActivity.java
+++ app/src/main/java/com/scy/courseselection/AllStuActivity.java
@@ -1,13 +1,18 @@
 package com.scy.courseselection;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -30,7 +35,7 @@ import okhttp3.Call;
 
 import static com.scy.courseselection.MainActivity.api_url;
 
-public class AllStuActivity extends AppCompatActivity implements SpringView.OnFreshListener {
+public class AllStuActivity extends AppCompatActivity implements SpringView.OnFreshListener,View.OnClickListener,AdapterView.OnItemLongClickListener {
     private Context context = this;
     private SpringView sv;
     private int page = 1;
@@ -39,6 +44,7 @@ public class AllStuActivity extends AppCompatActivity implements SpringView.OnFr
     List<HashMap<String, Object>> mListData  = new ArrayList<HashMap<String, Object>>();;
     private StuAdapter stuAdapter;
     private static final String TAG = "AllStuActivity";
+    private FloatingActionButton floatingActionButton;
 
     private Handler handler = new Handler() {
         public void handleMessage(Message msg) {
@@ -69,6 +75,10 @@ public class AllStuActivity extends AppCompatActivity implements SpringView.OnFr
 
         mlistview = (ListView)findViewById(R.id.booklist) ;
         mlistview.setDividerHeight(5 );
+        mlistview.setOnItemLongClickListener(this);
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(this);
 
         getData();
     }
@@ -150,5 +160,121 @@ public class AllStuActivity extends AppCompatActivity implements SpringView.OnFr
                         }
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab:{
+                View dialog_view = getLayoutInflater().inflate(R.layout.dialog_view_add_stu, null);
+                final EditText no = (EditText) dialog_view.findViewById(R.id.dialog_sno);
+                final EditText name = (EditText) dialog_view.findViewById(R.id.dialog_sname);
+                final EditText sex = (EditText) dialog_view.findViewById(R.id.dialog_sex);
+                final EditText age = (EditText) dialog_view.findViewById(R.id.dialog_age);
+                final EditText dept = (EditText) dialog_view.findViewById(R.id.dialog_dept);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("添加课程")
+                        .setView(dialog_view)
+                        .setCancelable(false)
+                        .setNegativeButton("取消",null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String content_no = no .getText().toString();
+                                String content_name = name .getText().toString();
+                                String content_sex = sex .getText().toString();
+                                String content_age = age .getText().toString();
+                                String content_dept = dept .getText().toString();
+                                addStu(content_no,content_name,content_sex,content_age,content_dept);
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+                break;
+            }
+        }
+    }
+
+    public void addStu(String no,String name,String sex,String age,String dept){
+        OkHttpUtils
+                .get()
+                .url(api_url)
+                .addParams("_action", "insertStu")
+                .addParams("sno", no)
+                .addParams("sname", name)
+                .addParams("ssex",sex)
+                .addParams("sage",age)
+                .addParams("sdept",dept)
+                .build()
+                .execute(new StringCallback()
+                {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Toast.makeText(context,"网络错误",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.d(TAG, "onResponse: "+response);
+                        try {
+                            JSONObject res = new JSONObject(response).getJSONObject("data");
+                            int status = res.getInt("status");
+                            if(status == 1)
+                                Toast.makeText(context,"添加成功",Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(context,"添加失败",Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context,"服务器错误",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        final String sno = (String) mListData.get(i).get("no");
+        new AlertDialog.Builder(this)
+                .setTitle("确定")
+                .setMessage("是否确认删除该学生")
+                .setCancelable(false)
+                .setNegativeButton("取消", null)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        OkHttpUtils
+                                .get()
+                                .url(api_url)
+                                .addParams("_action", "deleteStu")
+                                .addParams("sno", sno)
+                                .build()
+                                .execute(new StringCallback()
+                                {
+                                    @Override
+                                    public void onError(Call call, Exception e, int id) {
+                                        Toast.makeText(context,"网络错误",Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void onResponse(String response, int id) {
+                                        Log.d(TAG, "onResponse: "+response);
+                                        try {
+                                            JSONObject res = new JSONObject(response).getJSONObject("data");
+                                            int status = res.getInt("status");
+                                            if(status == 1)
+                                                Toast.makeText(context,"删除成功",Toast.LENGTH_LONG).show();
+                                            else
+                                                Toast.makeText(context,"删除失败",Toast.LENGTH_LONG).show();
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                            Toast.makeText(context,"服务器错误",Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });                    }
+                })
+                .create().show();
+        return true;
     }
 }
